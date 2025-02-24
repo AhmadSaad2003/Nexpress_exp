8ba13863d1e6c498fe16ecc22899dc2c2ef690db
Ahmad <ahmad.id.saad@gmail.com>
2025-02-24 16:41:03 +0100

    push before style

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -25,6 +25,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
 // Routes
 app.use('/api/auth/user', userRoute);
 app.use('/api/apps', appRoute);
